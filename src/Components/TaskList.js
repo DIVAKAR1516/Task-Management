0e26e069214af6cedca2b9630e2f5446418af4aa
DIVAKAR1516 <71762331016@cit.edu.in>
2026-03-28 10:26:59 +0530

    fix: add runtime and missing dependency

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import API from "../services/api";
+import './TaskList.css';
 
 function TaskList({ tasks, fetchTasks }) {
 
@@ -21,19 +22,18 @@ function TaskList({ tasks, fetchTasks }) {
   };
 
   return (
-    <div>
-      <h2>Tasks</h2>
-      {tasks.map((task) => (
-        <div key={task.id} style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
-          <h3>{task.title}</h3>
-          <p>{task.description}</p>
-          <p>Status: {task.status}</p>
-
-          <button onClick={() => updateStatus(task)}>Toggle Status</button>
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
-        </div>
-      ))}
+    <div className="task-list">
+  <h2>Tasks</h2>
+  {tasks.map((task) => (
+    <div key={task.id} className="task-card">
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+      <p className="status">Status: {task.status}</p>
+      <button onClick={() => updateStatus(task)}>Toggle Status</button>
+      <button onClick={() => deleteTask(task.id)}>Delete</button>
     </div>
+  ))}
+</div>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/", // change after deployment
+  baseURL: "https://task-management-2-2a87.onrender.com/",
+  //baseURL: "http://127.0.0.1:8000/api/", // change after deployment
 });
 
 export default API;
